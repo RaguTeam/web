2903d3ee1ebfc1dba543b079ac4cc64dc47d3ad5
m0tbka <razetkacraft2@mail.ru>
2026-07-15 18:08:47 +0300

    -

--- front/topbar/topbar.view.ts
+++ front/topbar/topbar.view.ts
@@ -1,21 +1,38 @@
 namespace $.$$ {
+  export class $raggu_web_front_topbar extends $.$raggu_web_front_topbar {
+    is_gallery() {
+      return this.screen() === "gallery";
+    }
+    is_explorer() {
+      return this.screen() === "explorer";
+    }
+    is_chat() {
+      return this.screen() === "chat";
+    }
+    is_dashboard() {
+      return this.screen() === "dashboard";
+    }
+    is_summary() {
+      return this.screen() === "summary";
+    }
 
-	export class $raggu_web_front_topbar extends $.$raggu_web_front_topbar {
+    no_dataset() {
+      return !this.dataset_id();
+    }
 
-		is_gallery() { return this.screen() === 'gallery' }
-		is_explorer() { return this.screen() === 'explorer' }
-		is_chat() { return this.screen() === 'chat' }
-		is_dashboard() { return this.screen() === 'dashboard' }
-		is_summary() { return this.screen() === 'summary' }
-
-		no_dataset() { return !this.dataset_id() }
-
-		@$mol_action click_gallery() { this.screen( 'gallery' ); return null }
-		@$mol_action click_explorer() { this.screen( 'explorer' ); return null }
-		@$mol_action click_chat() { this.screen( 'chat' ); return null }
-		@$mol_action click_dashboard() { this.screen( 'dashboard' ); return null }
-		@$mol_action click_summary() { this.screen( 'summary' ); return null }
-
-	}
-
+    @$mol_action click_gallery() {
+      this.screen("gallery");
+      return null;
+    }
+    @$mol_action click_explorer() {
+      this.screen("explorer");
+      return null;
+    }
+    // @$mol_action click_chat() { this.screen( 'chat' ); return null }
+    // @$mol_action click_dashboard() { this.screen( 'dashboard' ); return null }
+    @$mol_action click_summary() {
+      this.screen("summary");
+      return null;
+    }
+  }
 }
